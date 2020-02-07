fde6bfdb58320bd35e23752198ecc7eb79288bd2
Ezequiel <edonovan@opuzen.com>
2020-02-06 19:27:28 -0800

    first commit with some stuff

---
+++ run.py
@@ -0,0 +1,38 @@
+# first of all import the socket library
+import socket, argparse
+
+def run_client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '0.0.0.0' #socket.gethostname()
+    port = 9999
+    s.connect((host, port))
+    msg = s.recv(1024)
+    print(msg.decode('ascii'))
+    s.close()
+
+def run_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket successfully created")
+    host = '0.0.0.0' #socket.gethostname()
+    port = 9999
+    s.bind((host, port))
+    print("socket binded to %s" % (port))
+
+    s.listen(5)
+    print("socket is listening")
+    while True:
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        msg = "Thank you for connecting"
+        c.send(msg.encode("ascii"))
+        c.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', help='Run server', action='store_true')
+    args = parser.parse_args()
+
+    if args.server:
+        run_server()
+    else:
+        run_client()
