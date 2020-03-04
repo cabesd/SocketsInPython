eb617a45cb1fa30c7d85549d9432334c591907c6
Jorge <nflux-jmartinez@nflux.ai>
2020-03-04 15:11:23 -0800

    Next test

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import argparse
 import time
 
 
-class peer():
-    is_running = True
-    _input = None
+class Peer:
+    _args = ""
     _available_commands = ['help', 'myip', 'myport', 'connect', 'list', 'terminate', 'send', 'exit']
+    _input = None
     connections = []
+    is_running = True
+    sockets = []
     timeout = 2
 
     def __init__(self, port):
@@ -16,16 +18,16 @@ class peer():
         # Used to get the real ip address of this machine
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(('8.8.8.8', 12000))
-        self.myip = s.getsockname()[0]
+        self.my_ip = s.getsockname()[0]
         s.close()
 
-        self.myport = port
-        # self.open_socket()
-
-        # I guess this is the server
+        self.my_port = port
+        # Run the server
         t = Thread(None, self.check_inbox)
         t.start()
+        # Run the client
         self.run()
+        exit(0)
 
     def run(self):
         while self.is_running:
@@ -37,51 +39,62 @@ class peer():
                 getattr(self, 'func_' + self._args[0])()
 
     def func_help(self):
-        print("Show help message")
+        # print("Commands Usage Explanation")
+        pass
 
     def func_myip(self):
-        print(f"My IP is {self.myip}")
+        print(f"My IP is {self.my_ip}")
 
     def func_myport(self):
-        print(f"My port is {self.myport}")
+        print(f"My port is {self.my_port}")
 
     def func_connect(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(self._args)
         s.connect((self._args[1], int(self._args[2])))
-        self.connections.append(s)
+        self.sockets.append(s)
+        self.connections.append([self._args[1], int(self._args[2])])
         print(f"Connect {self._args}")
 
     def func_list(self):
-        print(f"Show Connection List\n{self.connections}")
+        """ Print all active connections """
+        print("ID\tIP Address\tPort No.")
+        for i, conn in enumerate(self.connections):
+            print(f"{i}:{conn[0]}\t{conn[1]}")
 
     def func_terminate(self):
+        """ Close all the connections """
         print(f"Terminate {self._args}")
 
     def func_send(self):
-        print(f"Send {self._args}")
+        """ Send a message """
+        idx = int(self._args[1])
+        msg = self._args[1:]
+        self.sockets[idx].send(msg)
 
     def func_exit(self):
-        '''TODO Send message to connections so they update their connections list'''
+        """ TODO Send message to connections so they update their connections list """
         self.is_running = False
-
-    def open_socket(self):
-        pass
 
     def check_inbox(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('0.0.0.0', int(self.myport)))
+        # Listens to any ip on this network on specified port
+        s.bind(('0.0.0.0', int(self.my_port)))
         while self.is_running:
 
-            # I think here we want to be able to accept as many connections as possible
-
-            s.listen(1024)
+            """ Since the connection is thrown away I might be able to get away with this"""
+            s.listen(100)
             # print("Checking messages from {}".format(self.connections))
-            time.sleep(1)
+            c, addr = s.accept()
+            print("Message received from {addr}")
+            msg = c.recv()
+            print(msg.decode("ascii"))
+            c.close()
+
+        exit(0)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("port", nargs=1)
     args = parser.parse_args()
-    peer(args.port[0])
+    Peer(args.port[0])
