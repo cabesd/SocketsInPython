44b45b4b024293428e9689326dc5b2e8a75d39fb
Jorge <nflux-jmartinez@nflux.ai>
2020-03-04 01:58:51 -0800

    lets try a test

--- run.py
+++ run.py
@@ -1,23 +1,31 @@
 from threading import Thread
-import socket, argparse, time
+import socket
+import argparse
+import time
+
 
 class peer():
     is_running = True
-    myip = '127.0.0.1'
-    myport = None
     _input = None
     _available_commands = ['help', 'myip', 'myport', 'connect', 'list', 'terminate', 'send', 'exit']
     connections = []
     timeout = 2
 
     def __init__(self, port):
-        self.myport = port
-        self.open_socket()
 
+        # Used to get the real ip address of this machine
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 12000))
+        self.myip = s.getsockname()[0]
+        s.close()
+
+        self.myport = port
+        # self.open_socket()
+
+        # I guess this is the server
         t = Thread(None, self.check_inbox)
         t.start()
         self.run()
-        t.cancel()
 
     def run(self):
         while self.is_running:
@@ -26,31 +34,32 @@ class peer():
             if self._args[0] not in self._available_commands:
                 print("Invalid command '{}' - type 'help' to get the available commands".format(self._input))
             else:
-                getattr(self, 'func_'+self._args[0])()
-
+                getattr(self, 'func_' + self._args[0])()
 
     def func_help(self):
         print("Show help message")
 
     def func_myip(self):
-        print("My IP is {}".format(self.myip))
+        print(f"My IP is {self.myip}")
 
     def func_myport(self):
-        print("My port is {}".format(self.myport))
+        print(f"My port is {self.myport}")
 
     def func_connect(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(self._args)
+        s.connect((self._args[1], int(self._args[2])))
         self.connections.append(s)
-        print("Connect {}".format(self._args))
+        print(f"Connect {self._args}")
 
     def func_list(self):
-        print("Show Connection List\n{}".format(self.connections))
+        print(f"Show Connection List\n{self.connections}")
 
     def func_terminate(self):
-        print("Terminate {}".format(self._args))
+        print(f"Terminate {self._args}")
 
     def func_send(self):
-        print("Send {}".format(self._args))
+        print(f"Send {self._args}")
 
     def func_exit(self):
         '''TODO Send message to connections so they update their connections list'''
@@ -61,8 +70,14 @@ class peer():
 
     def check_inbox(self):
         while self.is_running:
+
+            # I think here we want to be able to accept as many connections as possible
+
+
+            #s.listen('', port)
             print("Checking messages from {}".format(self.connections))
             time.sleep(3)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
