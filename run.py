714667b43db90cc3b73619177beba10a03302476
Jorge <nflux-jmartinez@nflux.ai>
2020-03-04 15:43:44 -0800

    terminate, list, help functional

--- run.py
+++ run.py
@@ -39,14 +39,21 @@ class Peer:
                 getattr(self, 'func_' + self._args[0])()
 
     def func_help(self):
-        # print("Commands Usage Explanation")
-        pass
+        print("Command\t\t\t\t\tDescription")
+        print("help\t\t\t\t\tPulls this up.")
+        print("myyp\t\t\t\t\tPrints your computer's ip address.")
+        print("myport\t\t\t\t\tPrints the port this program is communicating through.")
+        print("connect<destination><port>\t\tCreates new connection to specified destination at the specified port.")
+        print("list\t\t\t\t\tDisplays a numbered list of connections.")
+        print("terminate<connection id>\t\tTerminates connection specified by id in numbered list of connections.")
+        print("send<connection_id><message>\t\tSends message to specified peer by id in numbered list of connections.")
+        print("exit\t\t\t\t\tCloses all connections and terminates this process.")
 
     def func_myip(self):
         print(f"My IP is {self.my_ip}")
 
     def func_myport(self):
-        print(f"My port is {self.my_port}")
+        print(f"My Port is {self.my_port}")
 
     def func_connect(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,23 +66,31 @@ class Peer:
         """ Print all active connections """
         print("ID\tIP Address\tPort No.")
         for i, conn in enumerate(self.connections):
-            print(f"{i}:{conn[0]}\t{conn[1]}")
+            print(f"{i}:\t{conn[0]}\t{conn[1]}")
 
     def func_terminate(self):
-        """ Close all the connections """
-        print(f"Terminate {self._args}")
+        """ Close a connection specified by the connection id """
+        idx = int(self._args[1])
+        print("before", self.sockets)
+        self.sockets[idx].close()
+        self.sockets.pop(idx)
+        print("after", self.sockets)
+        self.connections.pop(idx)
+
+        print(f"Terminated {self._args}")
 
     def func_send(self):
-        """ Send a message """
+        """ Send a message to a specific connection id"""
         idx = int(self._args[1])
         msg = self._args[1:]
         self.sockets[idx].send(msg)
 
     def func_exit(self):
-        """ TODO Send message to connections so they update their connections list """
+        """ Close all the connections and then exit"""
         self.is_running = False
 
     def check_inbox(self):
+        """ Where the server is hosted"""
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # Listens to any ip on this network on specified port
         s.bind(('0.0.0.0', int(self.my_port)))
@@ -85,7 +100,7 @@ class Peer:
             s.listen(100)
             # print("Checking messages from {}".format(self.connections))
             c, addr = s.accept()
-            print("Message received from {addr}")
+            print(f"Data received {c} \n {addr}")
             msg = c.recv(100)
             print(msg.decode("ascii"))
             c.close()
